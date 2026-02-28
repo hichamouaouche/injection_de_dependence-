d8ddf2ac10eae6399ce89286a900eb6d70e01730
hichamouaouche <hichamouaouche@gmail.com>
2026-02-28 21:13:12 +0000

    new 30 min

--- src/main/java/hiou/hicham/pres/Pres2.java
+++ src/main/java/hiou/hicham/pres/Pres2.java
@@ -1,18 +1,25 @@
 package hiou.hicham.pres;
 
 import hiou.hicham.dao.IDao;
+import hiou.hicham.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 public class Pres2 {
-    public  static void main(String[] args ) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public  static void main(String[] args ) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
-        IDao dao =(IDao) cDao.newInstance();
+        IDao d =(IDao) cDao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cDao.getConstructor(IDao.class).newInstance(d);
+
 
 
     }
